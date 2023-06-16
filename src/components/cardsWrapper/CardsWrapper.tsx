c0a8e1496355ab17dfbd1b51221e308e3a73e50b
Maksimenko Maksim <mskmee2@gmail.com>
2023-06-16 05:15:46 +0300

    feat: portfolio page

--- src/components/cardsWrapper/CardsWrapper.tsx
+++ src/components/cardsWrapper/CardsWrapper.tsx
@@ -1,3 +1,13 @@
+import { portfolioData } from '../../utils/data';
+import { CardItem } from '../cardItem/CardItem';
+import styles from './CardsWrapper.module.css';
+
 export const CardsWrapper = () => {
-  return <div>CardsWrapper</div>;
+  return (
+    <div className={styles.wrapper}>
+      {portfolioData.map((card) => (
+        <CardItem key={crypto.randomUUID()} {...card} />
+      ))}
+    </div>
+  );
 };
--- src/pages/portfolio/Portfolio.tsx
+++ src/pages/portfolio/Portfolio.tsx
@@ -1,3 +1,11 @@
+import { CardsWrapper } from '../../components';
+import styles from './Portfolio.module.css';
+
 export const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return (
+    <div className={styles.portfolio}>
+      <h1 className={styles.title}>Portfolio</h1>
+      <CardsWrapper />
+    </div>
+  );
 };
